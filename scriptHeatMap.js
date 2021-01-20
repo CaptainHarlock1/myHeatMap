d9e5fc7ab7d1315818d0b6b839be3f0a8bad1a78
CaptainHarlock <anekav@gmail.com>
2021-01-20 16:59:27 +0200

    progressing

--- scriptHeatMap.js
+++ scriptHeatMap.js
@@ -13,8 +13,7 @@ req.onload = function(){
       generateScales();
       drawAxes();
       drawCells();
-
-           
+                       
 };
 //--
 
@@ -35,24 +34,36 @@ let data = [];
 //--
 
 
-//ορίζω τις χ-ψ κλίμακες μου - Define my x and y scales--
+//ορίζω κάποιες σταθερές που θα χρειαστώ για τα χ-ψ.
 let xScale
 let yScale
 
+let minYear
+let maxYear
+// let totalYears
 //--
 
 //ορίζω τις απαραίτητες λειτουργίες μου-Define my basic functions--
 let generateScales = ()=> {
+
+      minYear = d3.min(data, d => d.year)
+      maxYear = d3.max(data, d => d.year)
+
       xScale = d3.scaleLinear()
+                  .domain([minYear, maxYear])
                   .range([padding, w-padding])
       
       yScale = d3.scaleTime()
                   .domain([new Date(0, 0, 0, 0, 0, 0, 0), new Date(0, 12, 0, 0, 0, 0, 0)])
                   .range([padding, h-padding])
+
+      
+                  
 }
 
 let drawAxes = ()=> {
       let xAxis = d3.axisBottom(xScale)
+                        .tickFormat(d3.format('d'))
       let yAxis = d3.axisLeft(yScale)
 
       canvas.append("g")
@@ -92,6 +103,8 @@ let drawCells = ()=> {
             })
             .attr('height', (h-2*padding)/12)
             .attr('y', d => yScale(new Date(0, d.month-1, 0, 0, 0, 0, 0)))
+            .attr('width', ((w-(2*padding))/(maxYear-minYear)))
+            .attr('x', d => d.year)
 
 
 }
